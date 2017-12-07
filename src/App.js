453ce9d66f61055974b121dad61cbebb65a5895f
maverickalo <sean.vernon88@gmail.com>
2017-12-07 09:50:18 -0800

    Add Starter Files

---
+++ src/App.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+class App extends Component {
+  render() {
+    return (
+      <div>
+        <header>
+          <h1>Welcome to React</h1>
+        </header>
+        <p>Type Here to Get Started</p>
+      </div>
+    );
+  }
+}
+
+export default App;
